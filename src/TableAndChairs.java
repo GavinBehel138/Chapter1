8da78a3908d539eb869021f88047cdcd919a2743
Gavin Behel <behelgavin138@gmail.com>
2019-04-09 12:48:09 -0500

    update

---
+++ src/TableAndChairs.java
@@ -0,0 +1,26 @@
+public class TableAndChairs {
+    //Author: Brosius
+    //Date: 04/09/2019
+    //Project: Tables and Chairs
+
+    public static void main(String[] args) {
+
+        System.out.println("Building Some Tables And Chairs");
+        System.out.println("\n\n\n\n");
+
+        System.out.println("C                                      C");
+        System.out.println("H                                      H");
+        System.out.println("A                                      A");
+        System.out.println("I          TABLETABLETABLETABLE        I");
+        System.out.println("R            L           L             R");
+        System.out.println("CHAIRCHAIR   E           E    CHAIRCHAIR");
+        System.out.println("C        C   G           G    C        C");
+        System.out.println("H        H   L           L    H        H");
+        System.out.println("A        A   E           E    A        A");
+        System.out.println("I        I   G           G    I        I");
+        System.out.println("R        R   !           !    R        R");
+
+System.out.println("Look mom! i can commit to something");
+
+    }
+}
